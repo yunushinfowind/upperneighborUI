9911272fd3862a5087d590fb3570ea21c3a3c751
yunushinfowind <yunush.infowind@gmail.com>
2020-12-09 12:27:36 +0530

    change

--- src/app/admin/setting/setting.module.ts
+++ src/app/admin/setting/setting.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { SettingRoutingModule } from './setting-routing.module';
 import { EditComponent } from './edit/edit.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [EditComponent],
   imports: [
     CommonModule,
-    SettingRoutingModule
+    SettingRoutingModule , FormsModule , ReactiveFormsModule
   ]
 })
 export class SettingModule { }
